4bf8b2db91d113304477a981a95d836142c9399d
Roshan Thapa <roshan@Roshans-MacBook-Air-2.local>
2023-10-14 20:23:46 +0545

    initialized tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-row items-center justify-evenly h-[100vh] bg-gray-400">
+      <h1 className="font-serif text-4xl uppercase font-bold text-purple-800">Hello to my website</h1>
     </div>
   );
 }
