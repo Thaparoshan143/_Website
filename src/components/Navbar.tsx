a450748d04d0a807d24152a3084f78871b23caf5
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2024-11-13 10:35:52 +0545

    update: nav small screen responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import React from 'react'
 import { _navItems } from '../../public/data/_navItems'
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
+import { IoMdMenu, IoMdClose } from 'react-icons/io'
 
 interface INavItem
 {
@@ -9,15 +13,47 @@ interface INavItem
 };
 
 const Navbar = () => {
-  return (
-    <nav className="w-full z-10 p-2 py-3 flex flex-row justify-evenly items-center px-[20%] bg-theme-b backdrop-filter backdrop-blur-sm fixed top-0 left-0 shadow-xl ">
-        {
-            _navItems.map(({title, url}) => {
-                return <NavItem key={title} title={title} url={url} />
-            })
+    const [isNavOpen, setIsNavOpen] = useState<boolean>(true);
+    const [isMobileMenu, setIsMobileMenu] = useState<boolean>(false);
+
+    const toggleNav = () => {
+        setIsNavOpen((navVal) => (!navVal));
+    }
+
+    const handleResize = () => {
+        if(window.innerWidth < 640) {
+            setIsMobileMenu(true);
+            setIsNavOpen(false);
         }
-        <Link href={"#contact"} className="bg-theme-w text-theme-b hover:bg-theme-w-alt py-1 px-2 rounded-xl font-bold font-main-alt ml-[25%] transition-all duration-300">Get in Touch</Link>
-    </nav>
+        else if (window.innerWidth > 640)   {
+            setIsMobileMenu(false);
+            setIsNavOpen(true);
+        }
+    }
+
+    useEffect(() => {
+        handleResize();
+        window.addEventListener("resize", handleResize);
+    }, [])
+
+  return (
+        <>
+        <nav className={"w-full z-10 p-2 py-3 md:px-[20%] bg-theme-b backdrop-filter backdrop-blur-sm fixed top-0 left-0 shadow-xl " + (isNavOpen ? " flex sm:flex-row flex-col justify-evenly items-center sm:h-fit h-screen " : " hidden h-0 ")} onClick={() => setIsNavOpen((isMobileMenu ? false : true))}>
+            {
+                _navItems.map(({title, url}) => {
+                    return <NavItem key={title} title={title} url={url} />
+                })
+            }
+            <Link href={"#contact"} className="bg-theme-w text-theme-b hover:bg-theme-w-alt py-1 px-2 rounded-xl font-bold font-main-alt xl:ml-[25%] transition-all duration-300">Get in Touch</Link>
+        </nav>
+        {
+            isMobileMenu ? 
+            <button onClick={()=> toggleNav()} className={"bg-theme-w-alt hover:bg-theme-w p-2 rounded-[50%] hover:shadow-xl fixed top-5 right-5 z-20"}>
+            { !isNavOpen ? <IoMdMenu className="text-title text-theme" /> : <IoMdClose className="text-title text-theme "  />}
+            </button> 
+            : <></>
+        }
+        </>
   )
 }
 
